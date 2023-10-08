ce4f6b6b2f3229add689da66c6fc154d5422cf44
mizegit <dzm199@gmail.com>
2023-10-08 11:04:43 +0800

    init

---
+++ config/plugins.php
@@ -0,0 +1,13 @@
+<?php
+return [
+    'DebugKit' => [
+	    'forceEnable' => true
+    ],
+    'Bake' => [
+        'onlyCli' => true,
+        'optional' => true
+    ],
+    'Migrations' => [
+        'onlyCli' => true
+    ]
+];
